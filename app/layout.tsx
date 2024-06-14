85dfcc825b95fb5ae0fe6ca0e39f071156d54a1b
talhashafeeq-coder <talhashafeeq20202@gmail.com>
2024-06-14 14:18:51 +0500

    Create  wizard

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import RootProviders from "@/components/providers/RootProviders";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
               {/* <UserButton /> */}
             </SignedIn>
           </header>
+          <Toaster richColors position="bottom-right" />
           <RootProviders>{children}</RootProviders>
         </body>
       </html>
--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -3,20 +3,25 @@
 import { ReactNode } from 'react'
 import { ThemeProvider } from "next-themes";
 import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-function RootProviders({ children }: { children: ReactNode }) {           
+function RootProviders({ children }: { children: ReactNode }) {
   return (
     <div>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+
     </div>
   )
 }
-
-export default RootProviders
+export default RootProviders;
